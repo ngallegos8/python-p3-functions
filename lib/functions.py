bdd08f4f89871f70ae88831b11fa5defbfad9867
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2024-01-29 13:44:57 -0700

    lab complete. 7 tests passing

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    name = "Guido"
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+print(add(45,55))
 
 def halve(number):
-    pass
+    return number / 2
+print(halve(100))
+
+def my_function(param):
+    print("Running my_function")
+    return param + 1
+
+my_function_return_value = my_function(1)
+
+my_function_return_value
